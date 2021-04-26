0345fefe0fa8fec09162ccb5dc2a957273aa1b72
Blade <mjc@gabia.com>
2021-04-26 19:12:27 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ul>
+        <li><router-link v-bind:to="{ name: 'HelloWorld' }">HelloWorld</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-vue'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
