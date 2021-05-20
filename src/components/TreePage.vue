85794e384199aa4529424e1223016bd0c018b2d2
mjc <myungjae92@gmail.com>
2021-05-20 23:00:49 +0900

    search Panel 추가

--- src/components/TreePage.vue
+++ src/components/TreePage.vue
@@ -1,21 +1,49 @@
 <template>
-  <v-treeview
-    v-model='tree'
-    :open='initiallyOpen'
-    :items='items'
-    activatable
-    item-key='name'
-    open-on-click
+  <v-card
+    class="mx-auto"
+    v-model='showCard'
   >
-    <template v-slot:prepend='{ item, open }'>
-      <v-icon v-if='!item.file'>
-        {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
-      </v-icon>
-      <v-icon v-else>
-        {{ 'mdi-file-document' }}
-      </v-icon>
-    </template>
-  </v-treeview>
+    <v-sheet class="pa-4 primary lighten-2">
+      <v-text-field
+        v-model="search"
+        label="Search Company Directory"
+        dark
+        flat
+        solo-inverted
+        hide-details
+        clearable
+        clear-icon="mdi-close-circle-outline"
+      ></v-text-field>
+      <v-checkbox
+        v-model="caseSensitive"
+        dark
+        hide-details
+        label="Case sensitive search"
+      ></v-checkbox>
+    </v-sheet>
+    <v-card-text>
+      <v-treeview
+        v-model='tree'
+        :open='initiallyOpen'
+        :load-children='fetch'
+        :items='items'
+        :search='search'
+        :filter='filter'
+        activatable
+        item-key='name'
+        open-on-click
+      >
+        <template v-slot:prepend='{ item, open }'>
+          <v-icon v-if='!item.file'>
+            {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+          </v-icon>
+          <v-icon v-else>
+            {{ 'mdi-file-document' }}
+          </v-icon>
+        </template>
+      </v-treeview>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -26,15 +54,33 @@ export default {
     initiallyOpen: [],
     file: [],
     tree: [],
-    items: []
+    items: [],
+    search: null,
+    caseSensitive: false,
+    showCard: false
   }),
   created () {
     ipcRenderer.on('serverBindResponse', (event, searchEntryTree) => {
       this.items = []
       this.initiallyOpen = []
+      this.showCard = true
       this.items = Object.assign([], searchEntryTree)
       this.initiallyOpen = [searchEntryTree[0].name]
     })
+  },
+  computed: {
+    filter () {
+      return this.caseSensitive
+        ? (item, search, textKey) => {
+          return item[textKey].indexOf(search) > -1
+        }
+        : undefined
+    }
+  },
+  methods: {
+    fetch: (item) => {
+      console.log(item.data)
+    }
   }
 }
 </script>
