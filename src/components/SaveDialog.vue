5b86dd4c60f3aff10d086dd4cb01fe242c8d601b
mjc <myungjae92@gmail.com>
2022-03-01 12:46:24 +0900

    change-view 완성

--- src/components/SaveDialog.vue
+++ src/components/SaveDialog.vue
@@ -1,21 +1,28 @@
 <template>
-  <v-dialog max-width='600px' v-model='showDialog'>
+  <v-dialog max-width='600px' class='mx-auto' v-model='showDialog'>
     <v-card>
       <v-card-title>
-        <span class='headline'>Show Save List</span>
+        <span class='headline'>Show Changes</span>
       </v-card-title>
       <v-card-text>
-        <v-list>
+        <template v-if='changeAttrList.length > 0'>
+          <v-card-subtitle>
+            <v-icon small>mdi-circle-small</v-icon>
+            Attribute Changes
+          </v-card-subtitle>
+        </template>
+        <v-list expand>
           <v-list-group
             v-for='(item, index) in changeAttrList'
             append-icon=''
             prepend-icon=''
+            no-action
             :key='index'
             value='true'
           >
             <template v-slot:activator>
               <v-list-item-icon>
-                <v-icon color='yellow darken-2' >mdi-folder</v-icon>
+                <v-icon color='yellow darken-2'>mdi-folder</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
                 <v-list-item-title v-text="item.dn"></v-list-item-title>
@@ -30,19 +37,65 @@
                 :key='index'
                 append-icon=''
                 prepend-icon=''
+                no-action
                 sub-group
-                value='true'
               >
                 <template v-slot:activator>
                   <v-list-item-icon >
                     <v-icon color='green lighten-2'>mdi-file-document</v-icon>
                   </v-list-item-icon>
                   <v-list-item-content>
-                    <v-list-item-title>{{ changeData.type + " : " + changeData.values[0] }}</v-list-item-title>
+                    <v-list-item-title>
+                      {{ changeData.type + ' : ' + changeData.values[0] }}
+                      <span class='font-weight-bold red--text'>{{' (' + child.operation + ')'}}</span>
+                    </v-list-item-title>
                   </v-list-item-content>
                 </template>
               </v-list-group>
             </template>
+          </v-list-group>
+        </v-list>
+        <template v-if='modifyDnList.length > 0'>
+          <v-divider v-if='changeAttrList.length > 0'></v-divider>
+          <v-card-subtitle>
+            <v-icon small>mdi-circle-small</v-icon>
+            modifyDn Changes
+          </v-card-subtitle>
+        </template>
+        <v-list expand>
+          <v-list-group
+            v-for='(item, index) in modifyDnList'
+            append-icon=''
+            prepend-icon=''
+            :key='index'
+            value='true'
+          >
+            <template v-slot:activator>
+              <v-list-item-icon>
+                <v-icon color='yellow darken-2'>mdi-folder</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.originDn }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-group
+                append-icon=''
+                prepend-icon=''
+                no-action
+                sub-group
+              >
+                <template v-slot:activator>
+                  <v-list-item-icon >
+                    <v-icon color='yellow darken-2' >mdi-folder</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ item.modifyDn }}
+                      <span class='font-weight-bold red--text'>{{' (replace)'}}</span>
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </template>
+              </v-list-group>
           </v-list-group>
         </v-list>
         <v-spacer></v-spacer>
@@ -72,6 +125,7 @@ export default {
     ipcRenderer.on('returnShowSaveDialog', (event, modifyDnList, changeAttrList) => {
       this.modifyDnList = modifyDnList
       this.changeAttrList = changeAttrList
+      console.log(modifyDnList)
     })
   },
   computed: {
@@ -93,3 +147,15 @@ export default {
   }
 }
 </script>
+<style>
+v-application--is-ltr .v-list-item__action:first-child, .v-application--is-ltr .v-list-item__icon:first-child {
+  margin-right: 16px;
+}
+.v-card__subtitle {
+  padding: 16px;
+  padding-left: 0px;
+}
+.change {
+  font: bold;
+}
+</style>
