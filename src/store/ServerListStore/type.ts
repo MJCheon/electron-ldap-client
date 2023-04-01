c0d0a7ef7d84a07fa152ffb88ecfc218bc3c7df3
mjc <myungjae92@gmail.com>
2023-04-01 12:59:12 +0900

    Initialize
    Signed-off-by: mjc <myungjae92@gmail.com>

--- src/store/ServerListStore/type.ts
+++
@@ -1,9 +0,0 @@
-import { LdapConfig } from '@/library/common'
-
-export type LdapServerList = LdapServerConfig[]
-
-export interface LdapServerConfig extends LdapConfig {
-  id: string
-  name: string
-  iv: Buffer
-}
